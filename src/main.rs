8df8c5cd06e7f2f0c865742fc5992767a4b9a762
Nico Lube <nico@primecodes.de>
2024-09-12 17:36:32 +0200

    Add comments and input file arg

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 use std::{
-    collections::HashMap,
-    fs::File,
-    sync::{Arc, Mutex},
-    thread::{self},
+    collections::HashMap, env::args, fs::File, sync::{Arc, Mutex}, thread::{self}
 };
 
 #[derive(Debug, Clone)]
@@ -74,16 +71,19 @@ impl Chunk {
     }
 
     fn parse_line(&mut self) -> bool {
-        // Split at symicolon
+        // Find next semicolon, skipped 3 bytes because town is at least 3 bytes
         let split_pos = find_next(&self.data, self.position+3, b';');
         let name = &self.data[self.position..split_pos];
+        // Find next newline, skipped 3 bytes because temp at lest 3 bytes (x.x)
         self.position = find_next(&self.data, split_pos + 3, b'\n') + 1;
         let value = self.parse_value(&self.data[split_pos + 1..self.position - 1]);
+        // Simple hash function, stolen from java
         let mut key: u64 = 0;
         for i in 0..name.len() {
             key *= 31;
             key += name[i] as u64;
         }
+        // Update or insert new result
         self.result
             .entry(key)
             .and_modify(|fu: &mut Result| fu.update(value))
@@ -94,9 +94,12 @@ impl Chunk {
     fn parse_value(&self, data: &[u8]) -> i32 {
         let neg = data[0] == b'-';
         let mut result: i32 = 0;
+        // bool can be converted to usize because it is always 0 or 1
         for i in neg as usize..data.len() - 2 {
+            // Convert ascii to int
             result = result * 10 + (data[i] - b'0') as i32;
         }
+        // Convert to decimal (it's faster then using floats)
         result = result * 10 + (data[data.len() - 1] - b'0') as i32;
         if neg {
             -result
@@ -107,18 +110,25 @@ impl Chunk {
 }
 
 fn main() {
-    let file = File::open("measurements.txt").unwrap();
+    let file_name = args().nth(1);
+    if file_name.is_none() {
+        eprintln!("Usage: {} <file>", args().next().unwrap());
+        return;
+    }
+    let file_name = file_name.unwrap();
+    let file = File::open(file_name).unwrap();
 
+    // Map file to memory
     let mmaped = unsafe { memmap::Mmap::map(&file).unwrap() };
-
     let mmaped = Arc::new(mmaped);
 
     let max_threads: usize = thread::available_parallelism().unwrap().into();
 
+    // Calculate chunk size for each thread
     let chunk_size = mmaped.len() / max_threads;
 
+    // Split file into chunks by finding newlines at the end of each chunk
     let mut chunks = Vec::new();
-
     let mut next_start = 0;
     while next_start < mmaped.len() {
         let mut next_end = find_next(&mmaped, next_start + chunk_size, b'\n');
@@ -130,6 +140,7 @@ fn main() {
         next_start = next_end + 1;
     }
 
+    // Start threads for each chunk
     let mut threads = Vec::new();
     for chunk in &chunks {
         let chunk = chunk.clone();
@@ -154,10 +165,9 @@ fn main() {
                 .or_insert_with(|| value.clone());
         }
     }
-    let mut result = result
+    let result = result
         .iter()
         .map(|(_, value)| value.to_string())
         .collect::<Vec<String>>();
-    result.sort();
     println!("{{{}}}", result.join(", "));
 }
