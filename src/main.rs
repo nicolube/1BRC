e1463b880f8555908085087adf65b1f5e8843293
Nico Lube <nico@primecodes.de>
2024-09-12 15:47:14 +0200

    Replace Map with faster solution

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use std::{collections::HashMap, fs::File, sync::{Arc, Mutex}, thread::{self}};
+use std::{
+    collections::HashMap,
+    fs::File,
+    sync::{Arc, Mutex},
+    thread::{self},
+};
 
-
+#[derive(Debug, Clone)]
 struct Result {
+    name: String,
     min: i32,
     max: i32,
     mean: i64,
@@ -9,8 +15,9 @@ struct Result {
 }
 
 impl Result {
-    fn new() -> Self {
+    fn new(name: &[u8]) -> Self {
         Self {
+            name: String::from_utf8_lossy(name).to_string(),
             min: i32::MAX,
             max: i32::MIN,
             mean: 0,
@@ -36,13 +43,13 @@ impl Result {
         let min = self.min as f64 / 10.0;
         let max = self.max as f64 / 10.0;
         let mean = self.mean as f64 / self.count as f64 / 10.0;
-        format!("{:.1}/{:.1}/{:.1}", min, max, mean)
+        format!("{}={:.1}/{:.1}/{:.1}", self.name, min, max, mean)
     }
 }
 
 fn find_next(data: &[u8], mut position: usize, char: u8) -> usize {
     while position < data.len() && data[position] != char {
-        position += 1;  
+        position += 1;
     }
     return position;
 }
@@ -51,7 +58,7 @@ struct Chunk {
     data: Arc<memmap::Mmap>,
     end: usize,
     position: usize,
-    result: HashMap<Vec<u8>, Result>
+    result: HashMap<u32, Result>,
 }
 
 impl Chunk {
@@ -69,8 +76,12 @@ impl Chunk {
         let split_pos = find_next(&self.data, self.position, b';');
         let name = &self.data[self.position..split_pos];
         self.position = find_next(&self.data, split_pos + 3, b'\n') + 1;
-        let value =  self.parse_value(&self.data[split_pos + 1..self.position - 1]);
-        self.result.entry(name.to_vec()).or_insert(Result::new()).update(value);
+        let value = self.parse_value(&self.data[split_pos + 1..self.position - 1]);
+        let key = name.iter().fold(0, |acc, &x| acc << 8 | x as u32);
+        self.result
+            .entry(key)
+            .and_modify(|fu: &mut Result| fu.update(value))
+            .or_insert_with(|| Result::new(name));
         return self.position < self.end;
     }
 
@@ -80,7 +91,7 @@ impl Chunk {
         for i in neg as usize..data.len() - 2 {
             result = result * 10 + (data[i] - b'0') as i32;
         }
-        result = result * 10 + (data[data.len()-1] - b'0') as i32;
+        result = result * 10 + (data[data.len() - 1] - b'0') as i32;
         if neg {
             -result
         } else {
@@ -89,25 +100,22 @@ impl Chunk {
     }
 }
 
-
 fn main() {
     let file = File::open("measurements.txt").unwrap();
 
-    let mmaped = unsafe {
-        memmap::Mmap::map(&file).unwrap()
-    };
+    let mmaped = unsafe { memmap::Mmap::map(&file).unwrap() };
 
     let mmaped = Arc::new(mmaped);
-    
+
     let max_threads: usize = thread::available_parallelism().unwrap().into();
 
     let chunk_size = mmaped.len() / max_threads;
-    
+
     let mut chunks = Vec::new();
 
     let mut next_start = 0;
     while next_start < mmaped.len() {
-        let mut next_end = find_next(&mmaped,  next_start + chunk_size, b'\n');
+        let mut next_end = find_next(&mmaped, next_start + chunk_size, b'\n');
         if next_end > mmaped.len() {
             next_end = mmaped.len();
         }
@@ -121,8 +129,7 @@ fn main() {
         let chunk = chunk.clone();
         threads.push(thread::spawn(move || {
             let mut chunk = chunk.lock().unwrap();
-            while chunk.parse_line() {
-            }
+            while chunk.parse_line() {}
         }));
     }
     // Await all threads
@@ -132,14 +139,19 @@ fn main() {
 
     // Merge results
     let mut result = HashMap::new();
-    for chunk in &chunks{
+    for chunk in &chunks {
         let chunk = chunk.lock().unwrap();
         for (key, value) in &chunk.result {
-            result.entry(key.clone()).or_insert(Result::new()).merge(&value);
+            result
+                .entry(key.clone())
+                .and_modify(|fu: &mut Result| fu.merge(value))
+                .or_insert_with(|| value.clone());
         }
     }
-    let result: String= result.iter().map(|(key, value)| {
-        format!("{}={}", String::from_utf8_lossy(key), value.to_string())
-    }).collect::<Vec<String>>().join(", ");
-    println!("{{{}}}", result);
+    let mut result = result
+        .iter()
+        .map(|(_, value)| value.to_string())
+        .collect::<Vec<String>>();
+    result.sort();
+    println!("{{{}}}", result.join(", "));
 }
